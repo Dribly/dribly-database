6da744f3af80ce2ebf8fdb6480b5d7e0744054ce
Toby Powell-Blyth <toby@thegiltclub.com>
2017-11-16 12:13:53 +0000

    User model needs to look more like a user model

--- app/Dribly/User.php
+++ app/Dribly/User.php
@@ -23,4 +23,67 @@ class User extends DriblyModel
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName():string
+    {
+        return 'email';
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword():string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+    
 }
